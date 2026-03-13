d7c0cd7b405d98424cdf9dbb0224e9b17cf33a47
Kamil <lekamilthemage@gmail.com>
2026-03-13 18:45:55 +0100

    Runes: add easter egg effect texts, show hint for max-tier runes

--- Mods/Fishing_06adfaf8-00ed-44ee-ba94-9ece5f0f4752/Story/RawFiles/Lua/Fishing/Runes/Client.lua
+++ Mods/Fishing_06adfaf8-00ed-44ee-ba94-9ece5f0f4752/Story/RawFiles/Lua/Fishing/Runes/Client.lua
@@ -1,21 +1,72 @@
 
 local Tooltip = Client.Tooltip
+local Fishing = Epip.GetFeature("Features.Fishing")
 
 local Runes = Epip.GetFeature("Features.Fishing.Runes")
 local TSK = Runes.TranslatedStrings
+
+-- Maps fish ID to map of rune tiers to additional text to show within the rune power tooltip.
+---@type table<string, table<integer, {TSK: TextLib_TranslatedString, FormatArgs: any[]}>>
+Runes.RUNE_EXTRA_LABELS = {
+    ["Epipe"] = {
+        [1] = {
+            TSK = TSK.Effect_Epipe,
+            FormatArgs = {45},
+        },
+        [2] = {
+            TSK = TSK.Effect_Epipe,
+            FormatArgs = {120},
+        },
+        [3] = {
+            TSK = TSK.Effect_Epipe,
+            FormatArgs = {1337},
+        },
+    },
+    ["DescentKey"] = {
+        [1] = {
+            TSK = TSK.Effect_DescentKey,
+            FormatArgs = {20},
+        },
+        [2] = {
+            TSK = TSK.Effect_DescentKey,
+            FormatArgs = {55},
+        },
+        [3] = {
+            TSK = TSK.Effect_DescentKey,
+            FormatArgs = {100},
+        },
+    },
+    ["FidgetSpinner"] = {
+        [1] = {
+            TSK = TSK.Effect_FidgetSpinner,
+            FormatArgs = {5},
+        },
+        [2] = {
+            TSK = TSK.Effect_FidgetSpinner,
+            FormatArgs = {15},
+        },
+        [3] = {
+            TSK = TSK.Effect_FidgetSpinner,
+            FormatArgs = {25},
+        },
+    },
+}
 
 ---------------------------------------------
 -- EVENT LISTENERS
 ---------------------------------------------
 
--- Rebrand rune tooltips for fish and show a "Cannot equip" warning for unusable rune slots.
+-- Rebrand rune tooltips for fish, show extra effects and a "Cannot equip" warning for unusable rune slots.
 Tooltip.Hooks.RenderItemTooltip:Subscribe(function (ev)
     local item = ev.Item
-    if Runes.IsFishRune(item) then
+    local runeTier = Runes.GetFishRuneTier(item)
+    if runeTier then
+        local fish = Fishing.GetFishByTemplate(item.RootTemplate.Id)
+
         -- Rebrand rune slot labels to "Fish Essence"
         local runeSlotElement = ev.Tooltip:GetFirstElement("ArmorSlotType")
         runeSlotElement.Label = TSK.Label_FishRune:Format({
-            FormatArgs = {Runes.GetFishRuneTier(item)},
+            FormatArgs = {runeTier},
         })
 
         -- Show warning for unusable slots
@@ -24,7 +75,31 @@ Tooltip.Hooks.RenderItemTooltip:Subscribe(function (ev)
             local runeLabel = runeElement["Rune" .. i]
             if runeLabel == "" then
                 runeElement["Rune" .. i] = TSK.Label_CannotEquip:Format({Size = 18})
+            else
+                -- Add extra labels
+                if fish then
+                    local extraLabels = Runes.RUNE_EXTRA_LABELS[fish.ID]
+                    if extraLabels then
+                        local label = extraLabels[runeTier]
+                        if label then
+                            -- Note: some vanilla labels (ex. status applications) do not add a line break after them - thus we must check for it.
+                            local existingLabel = runeElement["Rune" .. i]
+                            local infix = string.find(existingLabel, "<br>$") and "" or "<br>"
+                            runeElement["Rune" .. i] = runeElement["Rune" .. i] .. infix .. label.TSK:Format({
+                                FormatArgs = label.FormatArgs
+                            })
+                        end
+                    end
+                end
             end
+        end
+
+        -- Add label for max-tier runes
+        if runeTier == #fish.RootTemplates then
+            local descElement = ev.Tooltip:GetFirstElement("ItemDescription")
+            descElement.Label = descElement.Label .. "<br><br>" .. TSK.Label_Descended:Format({
+                Color = Color.LARIAN.GREEN,
+            })
         end
     end
 end)
--- Mods/Fishing_06adfaf8-00ed-44ee-ba94-9ece5f0f4752/Story/RawFiles/Lua/Fishing/Runes/Shared.lua
+++ Mods/Fishing_06adfaf8-00ed-44ee-ba94-9ece5f0f4752/Story/RawFiles/Lua/Fishing/Runes/Shared.lua
@@ -14,6 +14,27 @@ local Runes = {
             Text = "Cannot equip.",
             ContextDescription = [[Tooltip for invalid rune slots for fish runes]],
         },
+        Label_Descended = {
+            Handle = "h95783f9cgad6bg4e87g87f6g8aaab72b4f1e",
+            Text = "This fish cannot descend any further.",
+            ContextDescription = [[Label for max-tier fish runes]],
+        },
+
+        Effect_Epipe = {
+            Handle = "h4bdb1662g0072g4fb6gb536g1d79d4f25e05",
+            Text = "+%s%% Quality-of-life",
+            ContextDescription = [[Easter egg tooltip for Epipe fish rune; param is amount as percentage]],
+        },
+        Effect_DescentKey = {
+            Handle = "ha6a7d3cdg3dfcg4defg8946g5f355d78e773",
+            Text = "+%s%% Epic Encounters",
+            ContextDescription = [[Easter egg tooltip for Descent Key fish rune; param is amount as percentage]],
+        },
+        Effect_FidgetSpinner = {
+            Handle = "h759cdefdg837dg46cdg94d3g32d7808c597c",
+            Text = "-%s%% Attention span",
+            ContextDescription = [[Easter egg tooltip for Fidget Spinner fish rune; param is amount as percentage]],
+        },
     }
 }
 Epip.RegisterFeature("Fishing.Runes", Runes)
