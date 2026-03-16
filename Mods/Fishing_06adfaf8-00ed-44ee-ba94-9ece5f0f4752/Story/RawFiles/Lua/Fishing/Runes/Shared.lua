14954786e3964216aa353825bd6e779a407e72da
Kamil <lekamilthemage@gmail.com>
2026-03-16 21:51:36 +0100

    Runes: add GetEquippedRunTier(), GetFish() and stats object overloads to other methods

--- Mods/Fishing_06adfaf8-00ed-44ee-ba94-9ece5f0f4752/Story/RawFiles/Lua/Fishing/Runes/Shared.lua
+++ Mods/Fishing_06adfaf8-00ed-44ee-ba94-9ece5f0f4752/Story/RawFiles/Lua/Fishing/Runes/Shared.lua
@@ -39,23 +39,70 @@ local Runes = {
 }
 Epip.RegisterFeature("Fishing.Runes", Runes)
 
+---Returns whether value is an extender Item object.
+---@param value any
+---@return boolean
+local function IsItemObject(value)
+    return GetExtType(value) == "EclItem" or GetExtType(value) == "EsvItem"
+end
+
 ---------------------------------------------
 -- METHODS
 ---------------------------------------------
 
+---Returns the fish associated with a fish rune.
+---@param rune Item|StatsLib_StatsEntry_Object
+---@return Features.Fishing.Fish? --`nil` if the item is not a fish rune.
+function Runes.GetFish(rune)
+    local templateID
+    if IsItemObject(rune) then
+        ---@cast rune Item
+        templateID = rune.RootTemplate.Id
+    else -- Stat object overload.
+        ---@cast rune StatsLib_StatsEntry_Object
+        templateID = rune.RootTemplate
+    end
+    return Fishing.GetFishByTemplate(templateID)
+end
+
 ---Returns whether an item is a fish rune.
----@param item Item
+---@param item Item|StatsLib_StatsEntry_Object
 ---@return boolean
 function Runes.IsFishRune(item)
-    return Fishing.GetFishByTemplate(item.RootTemplate.Id) ~= nil
+    return Runes.GetFish(item) ~= nil
 end
 
 ---Returns the fish rune tier of an item.
----@param item Item
+---@param item Item|StatsLib_StatsEntry_Object
 ---@return integer? -- `nil` if the item is not a fish rune.
 function Runes.GetFishRuneTier(item)
-    local fish = Fishing.GetFishByTemplate(item.RootTemplate.Id)
+    local fish = Runes.GetFish(item)
     if not fish then return nil end
-    local stats = item.StatsId
-    return Stats.Get("StatsLib_StatsEntry_Object", stats).RuneLevel
+    if IsItemObject(item) then
+        return Stats.Get("StatsLib_StatsEntry_Object", item.StatsId).RuneLevel
+    else -- Stat object overload.
+        ---@cast item StatsLib_StatsEntry_Object
+        return item.RuneLevel
+    end
+end
+
+---Returns the highest tier of a fish rune that is equipped by char, if any.
+---@param char Character
+---@param fish Features.Fishing.Fish|string
+---@return integer? -- `nil` if the fish rune is not equipped.
+function Runes.GetEquippedRuneTier(char, fish)
+    fish = type(fish) == "string" and Fishing.GetFish(fish) or fish
+    local equippedItems = Character.GetEquippedItems(char)
+    local highestTier = nil
+    for _,item in pairs(equippedItems) do
+        local runes = Item.GetRunes(item)
+        for _,rune in pairs(runes) do
+            local runeFish = Runes.GetFish(rune)
+            if runeFish and runeFish.ID == fish.ID then
+                local runeTier = Runes.GetFishRuneTier(rune)
+                highestTier = highestTier and math.max(highestTier, runeTier) or runeTier
+            end
+        end
+    end
+    return highestTier
 end
