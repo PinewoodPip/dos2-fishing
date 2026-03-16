23d40cee448b49ec5c49315c65c682714223d73f
Kamil <lekamilthemage@gmail.com>
2026-03-16 20:23:50 +0100

    Add warning if Epip is not installed

--- Mods/Fishing_06adfaf8-00ed-44ee-ba94-9ece5f0f4752/Story/RawFiles/Lua/BootstrapClient.lua
+++ Mods/Fishing_06adfaf8-00ed-44ee-ba94-9ece5f0f4752/Story/RawFiles/Lua/BootstrapClient.lua
@@ -1,7 +1,17 @@
 
--- Import Epip
+-- Check if Epip is loaded
+if not Ext.Mod.IsModLoaded("7d32cb52-1cfd-4526-9b84-db4867bf9356") then
+    Ext.Utils.ShowErrorAndExitGame("The Fishing mod requires Epip. See the website for installation instructions.<br>https://www.pinewood.team/epip/")
+    return
+end
+
+-- Import Epip and check version
 local EpipMod = Mods["EpipEncounters"]
 local Epip = EpipMod.Epip ---@type Epip
+if Epip.VERSION < 1075 then
+    Ext.Utils.ShowErrorAndExitGame("The Fishing mod requires Epip v1076 or higher. Please update!<br>https://www.pinewood.team/epip/")
+    return
+end
 Epip.ImportGlobals(_ENV)
 
 Ext.Require("BootstrapShared.lua")
