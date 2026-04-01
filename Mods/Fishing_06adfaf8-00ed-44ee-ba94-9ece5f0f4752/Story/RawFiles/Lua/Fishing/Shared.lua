0bff1ddca0bb32e89b650b59c91278e6c535cb27
Kamil <lekamilthemage@gmail.com>
2026-04-01 16:52:47 +0200

    Fishermancy: reduce total fish catches needed for early levels

--- Mods/Fishing_06adfaf8-00ed-44ee-ba94-9ece5f0f4752/Story/RawFiles/Lua/Fishing/Shared.lua
+++ Mods/Fishing_06adfaf8-00ed-44ee-ba94-9ece5f0f4752/Story/RawFiles/Lua/Fishing/Shared.lua
@@ -93,8 +93,8 @@ local Fishing = {
     },
 
     ABILITY_SCHOOL_COLOR = "86A4F7", ---@type htmlcolor
-    ABILITY_SCHOOL_FISH_PER_LEVEL = 15, -- Amount of fish that has to be caught per level in the Fishermancy ability.
-    ABILITY_SCHOOL_FISH_PER_LEVEL_GROWTH = 5, -- Extra amount of fish that need to be caught per level in the Fishermancy ability, for each level after the first.
+    ABILITY_SCHOOL_FISH_PER_LEVEL = 5, -- Amount of fish that has to be caught per level in the Fishermancy ability.
+    ABILITY_SCHOOL_FISH_PER_LEVEL_GROWTH = 6, -- Extra amount of fish that need to be caught per level in the Fishermancy ability, for each level after the first.
     ABILITY_SCHOOL_UNIQUE_FISH_PER_LEVEL = 5, -- Amount of unique fish that has to be caught per level in the Fishermancy ability.
 
     TARGET_POS_EFFECT_OFFSET = V(0, 1, 0),
