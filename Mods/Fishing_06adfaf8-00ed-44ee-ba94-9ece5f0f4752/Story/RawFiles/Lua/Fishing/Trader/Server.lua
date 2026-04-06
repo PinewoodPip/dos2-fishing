503862034b06900b1318ee6462c9fd0c81f59882
Kamil <lekamilthemage@gmail.com>
2026-04-06 11:24:52 +0200

    Trader: add map-specific greeting dialogue

--- Mods/Fishing_06adfaf8-00ed-44ee-ba94-9ece5f0f4752/Story/RawFiles/Lua/Fishing/Trader/Server.lua
+++ Mods/Fishing_06adfaf8-00ed-44ee-ba94-9ece5f0f4752/Story/RawFiles/Lua/Fishing/Trader/Server.lua
@@ -13,10 +13,32 @@ Osiris.RegisterSymbolListener("ObjectFlagSet", 3, "after", function (flag, speak
     if flag == Trader.DIALOG_FLAGS.OPEN_CODEX then
         local char = Character.Get(speakerGUID)
         Net.PostToCharacter(char, Trader.NETMSG_OPEN_CODEX)
+    elseif flag == Trader.DIALOG_FLAGS.SPOKEN_TO then
     end
 end)
 
--- Update collection log completion flags starting the dialogue.
+-- Update map-specific flags when starting the dialogue.
+Osiris.RegisterSymbolListener("DialogStarted", 2, "after", function (dialogID, instance)
+    if dialogID == Trader.DIALOG_ID then
+        local charGUID = Osi.DialogGetInvolvedPlayer(instance, 1)
+        local levelID = Entity.GetLevel().LevelDesc.LevelName
+
+        -- If speaking to the trader for the first time, set the level-specific spoken flag to avoid that dialogue from playing next time;
+        -- would be awkward otherwise, as they're written as though it's your first time meeting him there
+        if Osi.ObjectGetFlag(charGUID, Trader.DIALOG_FLAGS.SPOKEN_TO) == 0 then
+            Osi.ObjectSetFlag(charGUID, Trader.DIALOG_FLAGS.SPOKEN_IN_PREFIX .. levelID)
+        end
+        -- Clear any previous level flags
+        for _,otherLevelID in pairs(Entity.LEVEL_IDS.ORIGINS) do
+            Osi.GlobalClearFlag(Trader.DIALOG_FLAGS.REGION_PREFIX .. otherLevelID)
+        end
+
+        -- Set flag for current level
+        Osi.GlobalSetFlag(Trader.DIALOG_FLAGS.REGION_PREFIX .. levelID)
+    end
+end)
+
+-- Update collection log completion flags when starting the dialogue.
 Osiris.RegisterSymbolListener("DialogStarted", 2, "after", function (dialogID, instance)
     if dialogID == Trader.DIALOG_ID then
         local charGUID = Osi.DialogGetInvolvedPlayer(instance, 1)
--- Mods/Fishing_06adfaf8-00ed-44ee-ba94-9ece5f0f4752/Story/RawFiles/Lua/Fishing/Trader/Shared.lua
+++ Mods/Fishing_06adfaf8-00ed-44ee-ba94-9ece5f0f4752/Story/RawFiles/Lua/Fishing/Trader/Shared.lua
@@ -4,6 +4,9 @@ local Trader = {
     TRADER_ROOTTEMPLATE = "a093021b-46fc-4b6d-87dc-21ea6fa535b7",
     DIALOG_FLAGS = {
         OPEN_CODEX = "PIP_AskedAboutCollectionLog",
+        SPOKEN_TO = "PIP_WasSpokenToEver",
+        REGION_PREFIX = "PIP_Fishing_Map_", -- Meant to be suffixed with the level ID.
+        SPOKEN_IN_PREFIX = "PIP_Fishing_SpokenIn_", -- Meant to be suffixed with the level ID.
         COLLECTION_LOG_PROGRESS_PREFIX = "PIP_Fishing_CollectionLog_", -- Meant to be suffixed with the completion percentage, in increments of 10%.
     },
     DIALOG_ID = "PIP_Pip_FortJoy",
